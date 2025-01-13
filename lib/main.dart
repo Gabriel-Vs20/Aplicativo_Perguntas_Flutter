117a585c62cf46d1b35aec3285a6b4b7e42f9668
Gabriel-Vs20 <gabrielvalengasilva412@gmail.com>
2025-01-13 00:19:24 -0300

    Correção do erro do aplicativo acessar uma posição inválida na lista de perguntas e criação de uma nova tela, para o usuário recomeçar o programa.

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,6 @@ class _PerguntaAppState extends State <PerguntaApp>{
 
   var perguntasSelecionadas = 0;
 
-  var respostas = 'Resposta 1';
-  var respostasDois = 'Resposta 2';
-  var respostasTres = 'Resposta 3';
-  
-
-  void responder(){
-    setState(() {
-      perguntasSelecionadas++;
-      print(perguntasSelecionadas);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context){
-
    final perguntas = [
     {
         'texto' : 'Qual é a sua cor favorita?',
@@ -38,31 +23,86 @@ class _PerguntaAppState extends State <PerguntaApp>{
     }
       
     ];
-    List <String> respostas = perguntas[perguntasSelecionadas].cast() ['resposta'];
+  
+  void responder(){
+    setState(() {
+      perguntasSelecionadas++;
+      print(perguntasSelecionadas);
+    });
+  }
+
+   void recomecar(){
+    setState(() {
+      perguntasSelecionadas=  0;
+      print(perguntasSelecionadas);
+    });
+  }
+
+  bool get temPerguntaSelecionada
+  {
+    return perguntasSelecionadas < perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context){
+
+    List <String> respostas = temPerguntaSelecionada ? perguntas[perguntasSelecionadas].cast() ['resposta'] : [];
     List <Widget> widgets = [];
 
-    for (String textoResp in respostas){
-    widgets.add (Respostas (textoResp, responder));
-    }
+      for (String textoResp in respostas){
+      widgets.add(Respostas (textoResp, responder));
+     }
+  
       return MaterialApp(
 
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Perguntas!'),
+          title: Text('Quiz!'),
           titleTextStyle: TextStyle(color: Colors.blue,
           fontSize: 25,
           fontWeight: FontWeight.bold ),
           centerTitle: true,
           backgroundColor: const Color.fromARGB(255, 145, 194, 247), 
         ),
-        body: Column(
+        body: temPerguntaSelecionada ? Column(
           children: <Widget> [
             Questao(perguntas[perguntasSelecionadas]['texto'].toString()),
             ...widgets,
             //... pega todos os elementos da lista;
           ],
+        ): Column(
+          children: [
+            Padding(
+            padding: EdgeInsets.only(top: 40.0),
+            child: Text('Parabéns, você chegou ao fim das perguntas. Clique no botão abaixo para recomeçar',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+            color: Colors.blue,
+            fontSize: 25,
+            ),),
+            ),
+            
+            
+            Align(
+             alignment: Alignment.center, 
+              child: Padding(
+              padding: EdgeInsets.only(top: 20.0),
+                child: ElevatedButton(
+                onPressed: recomecar,
+                  child: Text('Recomeçar'),
+                  style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(250, 75)
+                )
 
-          
+              
+
+             ),
+             ),
+            )
+           
+          ],
         ),
 
         backgroundColor: const Color.fromARGB(255, 249, 249, 249),
